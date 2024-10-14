9204379fa2a6d0ef217e67e7793a32ec5590ad63
“eugenioBorras” <eugenio.borras5@gmail.com>
2024-10-14 20:59:00 -0300

    [Js]Agregar funcion editar patente

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
-let entrada =  prompt ("ingrese la opción deseada"+ "\n"+"1-Entra"+"\n"+"2-Sale"+"\n"+"3-Autos ingresados"+"\n"+"4-Resumen del día"+"\n"+"5-Salir");
+let entrada =  prompt ("ingrese la opción deseada"+ "\n"+"1-Entra"+"\n"+"2-Sale"+"\n"+"3-Editar Patente"+"\n"+"4-Autos ingresados"+"\n"+"5-Resumen del día"+"\n"+"6-Salir");
 let patente = 0;
-let clase = "";
 const autos = [];
 const resumen = [];
+let nuevaPatente = 0;
 
-
-while(entrada != "5"){
+while(entrada != "6"){
     
     switch(entrada){
         case "1":
             console.log("Ingresa vehículo")
             patente = prompt("ingresa los 3 números de la patente") ;
-            if(patente.length > 3 && patente.length < 3){
-                alert ("Error en formato de fatente, por favor ingrese nuevamente la patente")
-                patente = prompt("ingresa los 3 números de la patente") ;
-            }else
             ingresar(patente);
             break;
         case "2":
@@ -23,12 +18,17 @@ while(entrada != "5"){
             eliminar(patente);
             break;
         case "3":
+            console.log("Editar Patente");
+            patente = prompt("Ingresa la patente a editar");
+            editar(patente);
+            break;
+        case "4":
             console.log("Autos en playa");
             autos.sort();
             console.log(autos.join(" "+ "\n"));
-            console.log("Hay "+autos.length + "autos en playa");
+            console.log("Hay "+autos.length + " autos en playa");
             break;
-        case "4" :
+        case "5" :
             console.log("Resumen diario de vahículos");
             console.log(resumen.join(" "+ "\n"));
             break;
@@ -36,7 +36,7 @@ while(entrada != "5"){
         console.log("Error, ingrese una opcion válida");
             break;
     }
-    entrada =  prompt ("ingrese la opción deseada"+ "\n"+"1-Entra"+"\n"+"2-Sale"+"\n"+"3-Autos ingresados"+"\n"+"4-Resumen del día"+"\n"+"5-Salir");
+    entrada =  prompt ("ingrese la opción deseada"+ "\n"+"1-Entra"+"\n"+"2-Sale"+"\n"+"3-Modificar Patente"+"\n"+"4-Autos ingresados"+"\n"+"5-Resumen del día"+"\n"+"6-Salir");
 }
 
 function ingresar (patente) {
@@ -52,8 +52,19 @@ function ingresar (patente) {
 function eliminar (patente){
     if(autos.includes(patente)){
         if(autos.indexOf(patente) >= 0)
-            autos.splice(autos.indexOf(patente) , 1)
+            autos.splice(autos.indexOf(patente) , 1);
     }else{
-        console.log("el auto no se encuentra en la lista")
+        console.log("el auto no se encuentra en la lista");
+    }
+}
+
+function editar(patente){
+    if(autos.includes(patente)){
+        if(autos.indexOf(patente) >= 0)
+            nuevaPatente = prompt ("Ingrese la nueva patente")
+            autos.splice(autos.indexOf(patente) , 1, nuevaPatente);
+            resumen.splice(resumen.indexOf(patente) , 1, nuevaPatente);
+    }else{
+        console.log("el auto no se encuentra en la lista");
     }
 }
